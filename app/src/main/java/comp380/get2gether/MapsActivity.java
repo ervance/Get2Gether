73f565b1002b61442cd2a0a58ac5f8ba36f3ab67
Eric Vance <ericr.vance@gmail.com>
2016-02-16 21:55:44 -0800

    Made changes to the map:    - Added current location updater    - Added another marker that will point at the current location

--- app/src/main/java/comp380/get2gether/MapsActivity.java
+++ app/src/main/java/comp380/get2gether/MapsActivity.java
@@ -1,7 +1,12 @@
 package comp380.get2gether;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,10 +23,15 @@ import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    /******Map fields******/
     private GoogleMap mMap;
-    //coordinate list
-    private ArrayList<LatLng> coordinateList = new ArrayList<>();
-    private int listSize;
+    private ArrayList<LatLng> coordinateList = new ArrayList<>();  //collects coordinates
+    private int listSize;  //size of coordinateList
+    private LocationManager locManager;
+    private LatLng currentLocale;
+    private OnMapReadyCallback callback;
+
+    /****End Map fields****/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,10 +57,58 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng pdale = new LatLng(34.594812, -118.169495);
-        mMap.addMarker(new MarkerOptions().position(pdale).title("This is our test marker"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(pdale));
+        /**********Change UI settings****************/
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true); //permisions need to be set
+        mMap.getUiSettings().setMyLocationButtonEnabled(true); //false to disable
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setRotateGesturesEnabled(true);
+        // Add a marker in Northridge and move the camera
+        /*Consider putting the following code into a method to streamline the marker placing
+        and make it more reuseable
+        */
+        LatLng northRidge = new LatLng(34.2417, -118.5283);
+        mMap.addMarker(new MarkerOptions().position(northRidge).title("This is our test marker\nClass Comp380"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(northRidge));
+
+        //Get an updtated location
+        locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000L, 500.0f, locationListener);
+        Location location = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        //If a location is found move the marker and camera to the current location
+        if ( location != null )
+        {
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+            //create the location
+            currentLocale = new LatLng(latitude,longitude);
+            //add marker and move camera
+            mMap.addMarker(new MarkerOptions().position(currentLocale).title("Current Location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocale));
+        }//end if
+//        else
+//        {
+//            //try to get a location because none was found previously
+//            updateWithNewLocation(location);
+//        }
+
+        //wrapper around a view of a map to automatically handle the necessary life cycle needs
+        //Sets a callback object which will be triggered when the GoogleMap instance is ready to be used.
+        //May or may not need to use the OnMapReadyCallback here
+        //Turns out it was causing a problem so I took it out
+
+
     }
 
     //Location listener
@@ -77,9 +135,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         @Override
         public void onProviderDisabled(String provider) {
-
             updateWithNewLocation(null);
-
         }
 
     };
