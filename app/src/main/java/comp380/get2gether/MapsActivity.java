62603f213c2a0290bc91832585a64c3127636ed2
Dino <dino.biel.934@my.csun.edu>
2016-02-16 18:35:26 -0800

    Begin to add Location Listener and Location Update

--- app/src/main/java/comp380/get2gether/MapsActivity.java
+++ app/src/main/java/comp380/get2gether/MapsActivity.java
@@ -1,7 +1,10 @@
 package comp380.get2gether;
 
+import android.location.Location;
+import android.location.LocationListener;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +13,15 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    //coordinate list
+    private ArrayList<LatLng> coordinateList = new ArrayList<>();
+    private int listSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,5 +51,54 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng pdale = new LatLng(34.594812, -118.169495);
         mMap.addMarker(new MarkerOptions().position(pdale).title("This is our test marker"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(pdale));
+    }
+
+    //Location listener
+    //collect lat lngs and put into list
+    private final LocationListener locationListener = new LocationListener(){
+        @Override
+        public void onLocationChanged(Location location) {
+            updateWithNewLocation(location);
+            LatLng temp = new LatLng(location.getLatitude(), location.getLongitude());
+            coordinateList.add(temp);
+            listSize++;
+            Log.e("LocationListener", "Coordinates Added to list");
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+            updateWithNewLocation(null);
+
+        }
+
+    };
+
+    //New Location Updater
+
+    private void updateWithNewLocation(Location location) {
+        String latLongString = "";
+
+        if (location != null){
+            //create a string with current lat long
+            double lat = location.getLatitude();
+            double lng = location.getLongitude();
+            //latLongString = "Lat:" + lat + "\nLong:" + lng;
+        }
+
+            //THIS MAY NOT NEED TO BE HERE
+            //ORIGNIAL CODE HAD REDUNCANCY
+            //CHECK TO MAKE SURE
+
     }
 }
