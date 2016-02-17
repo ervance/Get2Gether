24d48eba2786aee059a16c3b75e878e7126d72aa
Dino <dino.biel.934@my.csun.edu>
2016-02-16 17:49:40 -0800

    Change marker location and pin name

--- app/src/main/java/comp380/get2gether/MapsActivity.java
+++ app/src/main/java/comp380/get2gether/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng pdale = new LatLng(34.594812, -118.169495);
+        mMap.addMarker(new MarkerOptions().position(pdale).title("This is our test marker"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(pdale));
     }
 }
