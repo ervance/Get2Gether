6ef11fee77f6876b54d89d71cecb2335517cd27b
Dino <dino.biel.934@my.csun.edu>
2016-02-18 01:15:56 -0800

    - Added FormActivty - This includes FormActivity.java (source code) - This also includes the GUI interface

--- app/src/main/java/comp380/get2gether/MapsActivity.java
+++ app/src/main/java/comp380/get2gether/MapsActivity.java
@@ -160,4 +160,5 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     //Code to drop pin Need to give Pin attributes to hold info from "FormAcitivity"
+
 }
