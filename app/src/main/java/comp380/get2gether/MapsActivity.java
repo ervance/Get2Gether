4cfbe63255cb86fa1374cc66347dccf8c095f53f
kjohnson7476 <k.johnson1950@gmail.com>
2016-02-23 17:05:37 -0800

    Nothing was changed...said keith

--- app/src/main/java/comp380/get2gether/MapsActivity.java
+++ app/src/main/java/comp380/get2gether/MapsActivity.java
@@ -1,7 +1,15 @@
 package comp380.get2gether;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +18,20 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    /******Map fields******/
     private GoogleMap mMap;
+    private ArrayList<LatLng> coordinateList = new ArrayList<>();  //collects coordinates
+    private int listSize;  //size of coordinateList
+    private LocationManager locManager;
+    private LatLng currentLocale;
+    private OnMapReadyCallback callback;
+
+    /****End Map fields****/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +57,104 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        /**********Change UI settings****************/
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true); //permisions need to be set
+        mMap.getUiSettings().setMyLocationButtonEnabled(true); //false to disable
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setRotateGesturesEnabled(true);
+        // Add a marker in Northridge and move the camera
+        /*Consider putting the following code into a method to streamline the marker placing
+        and make it more reuseable
+        */
+        LatLng northRidge = new LatLng(34.2417, -118.5283);
+        mMap.addMarker(new MarkerOptions().position(northRidge).title("This is our test marker\nClass Comp380"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(northRidge));
+
+        //Get an updtated location
+        locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000L, 500.0f, locationListener);
+        Location location = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        //If a location is found move the marker and camera to the current location
+        if ( location != null )
+        {
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+            //create the location
+            currentLocale = new LatLng(latitude,longitude);
+            //add marker and move camera
+            mMap.addMarker(new MarkerOptions().position(currentLocale).title("Current Location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocale));
+        }//end if
+//        else
+//        {
+//            //try to get a location because none was found previously
+//            updateWithNewLocation(location);
+//        }
+
+        //wrapper around a view of a map to automatically handle the necessary life cycle needs
+        //Sets a callback object which will be triggered when the GoogleMap instance is ready to be used.
+        //May or may not need to use the OnMapReadyCallback here
+        //Turns out it was causing a problem so I took it out
+
+
+    }
+
+    //Location listener
+    //collect lat lngs and put into list
+    private final LocationListener locationListener = new LocationListener(){
+        @Override
+        public void onLocationChanged(Location location) {
+            updateWithNewLocation(location);
+            LatLng temp = new LatLng(location.getLatitude(), location.getLongitude());
+            coordinateList.add(temp);
+            listSize++;
+            Log.e("LocationListener", "Coordinates Added to list");
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            updateWithNewLocation(null);
+        }
+
+    };
+
+    //New Location Updater
+
+    private void updateWithNewLocation(Location location) {
+        String latLongString = "";
+
+        if (location != null){
+            //create a string with current lat long
+            double lat = location.getLatitude();
+            double lng = location.getLongitude();
+            //latLongString = "Lat:" + lat + "\nLong:" + lng;
+        }
+
+            //THIS MAY NOT NEED TO BE HERE
+            //ORIGNIAL CODE HAD REDUNCANCY
+            //CHECK TO MAKE SURE
+
     }
 }
