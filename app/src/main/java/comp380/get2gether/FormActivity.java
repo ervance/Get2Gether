172e1f04f908998180b3c8709bf46d02c9a2d9b4
Dino <dino.biel.934@my.csun.edu>
2016-02-23 17:35:10 -0800

    New in this Update: -------------------------------------- custom_info_window.xml - This file holds the framwork for our popup window above the marker
    --Added functionality--
    - Added custom popup window to display information for events above the marker
    - Fixed a bug which prevented the data from being displayed correctly in the popup window
    - Added the functionality of dragging the marker to move it to a new location on the map

---
+++ app/src/main/java/comp380/get2gether/FormActivity.java
@@ -0,0 +1,71 @@
+package comp380.get2gether;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class FormActivity extends AppCompatActivity {
+
+    private EditText chosenEventName; //holds the event name from the eventName text box
+    private EditText chosenEventType; //Holds eventLocation ""
+    private EditText chosenEventTime;  //holds the eventTime ""
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Ties the completeForm button to the variable submitButton
+        Button submitButton =(Button) findViewById(R.id.completeForm);
+
+        //Create an onClickListener to save user input to variables and open MapActivity
+        submitButton.setOnClickListener(new View.OnClickListener() {
+
+            //This is for for the submit button on the form page
+            public void onClick(View v) {
+
+                //On button press, store text from the 3 fields
+                chosenEventName = (EditText) findViewById(R.id.eventName);
+                chosenEventType = (EditText) findViewById(R.id.eventType);
+                chosenEventTime = (EditText) findViewById(R.id.eventTime);
+
+                String eName = chosenEventName.getText().toString();
+                String eType = chosenEventType.getText().toString();
+                String eTime = chosenEventTime.getText().toString();
+
+                //Built Arraylist to store variables from Form
+                ArrayList<String> formVariables = new ArrayList<>();
+                formVariables.add(eName);
+                formVariables.add(eType);
+                formVariables.add(eTime);
+
+                //Toast is a pop up message on screen could be useful later...right now not important.
+                 //Toast toast = new Toast(getApplicationContext());
+                 //toast.setGravity(Gravity.TOP| Gravity.LEFT, 0, 0);
+                 //toast.makeText(FormActivity.this, chosenEventName.getText(), toast.LENGTH_SHORT).show();
+                //----------------------------------------------------------------------------------------
+
+                //Create an Intent in order to pass info to "MapsAcitivity"
+                Intent intent = new Intent(FormActivity.this, MapsActivity.class);
+
+
+                //Create Identifier for variable types in this .java file
+                //Make an arraylist instead of putExtra
+                intent.putExtra("formVar", formVariables);
+
+                //Start other Activity (MapsActivity) with pin
+                startActivity(intent);
+            }
+        });
+    }
+}
