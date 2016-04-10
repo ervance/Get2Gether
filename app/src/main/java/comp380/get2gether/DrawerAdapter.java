31eb4bbf4f1efae589d6d82a65cadb83125999fe
Eric Vance <ericr.vance@gmail.com>
2016-04-09 17:40:34 -0700

    *added a drawer to map *added activity to view after clicking on the popup window **Note: needs data specific to the activity and drawer, not representative of the final project.

---
+++ app/src/main/java/comp380/get2gether/DrawerAdapter.java
@@ -0,0 +1,74 @@
+package comp380.get2gether;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Eric on 4/9/2016.
+ */
+public class DrawerAdapter extends ArrayAdapter<String> {
+    //class to populate and create drawer
+
+    private final Context context;
+    private final List<String> drawerString;
+
+    public DrawerAdapter(Context context, List<String> drawerString) {
+        super(context, 0, drawerString);
+        this.context = context;
+        this.drawerString = drawerString;
+        //this.alMarkers = alMarkers;
+        //debugging log data
+//        for (int i = 0; i < alMarkers.size(); i++) {
+//            Log.i("MENU", "" + alMarkers.get(i).title + " " + alMarkers.get(i).imgId);
+//        }
+    }//end DrawerAdapter constructor
+
+    //@Override
+//    public int getCount() {
+//        return alMarkers.size();
+//    }
+
+    //@Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        View view = convertView;
+
+        //create the drawer and the view of it
+        if (view == null){
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            view = inflater.inflate(R.layout.drawer_list, parent, false);
+            holder = new ViewHolder();
+            holder.img = (ImageView) view.findViewById(R.id.drawer_list_img_view);
+            holder.title = (TextView) view.findViewById(R.id.drawer_list_text_view);
+            view.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder) view.getTag();
+        }
+
+//        //debugging log data
+//        Log.e("MENU", "" + alMarkers.get(position).title + " Position: " + position);
+//        for (int i = 0; i < alMarkers.size(); i++) {
+//            Log.e("MENU", "" + alMarkers.get(position).title + " " + alMarkers.get(position).imgId);
+//        }
+//        //end log
+        holder.img.setImageResource(R.drawable.holderpic);
+        holder.title.setText("ACTIVITY HOLDER");
+        return view;
+    }//end getView
+
+
+    private static class ViewHolder{
+        private TextView title;
+        private ImageView img;
+    }//end ViewHolder class
+}//end DrawerAdapter Class
