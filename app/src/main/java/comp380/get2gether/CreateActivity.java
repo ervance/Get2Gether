dcdc443ce66f928351aa9d0e7b6d8f8e0a1cc086
Dino <dino.biel.934@my.csun.edu>
2016-04-17 10:54:53 -0700

    Dino Updates -------------------- - Added Map Updating Functionality to mapfragment in createActivity

--- app/src/main/java/comp380/get2gether/CreateActivity.java
+++ app/src/main/java/comp380/get2gether/CreateActivity.java
@@ -27,6 +27,10 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -43,12 +47,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class CreateActivity extends FragmentActivity {
+public class CreateActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
     private EditText chosenEventName; //holds the event name from the eventName text box
     private EditText chosenEventType; //Holds eventLocation ""
     private EditText chosenEventTime;  //holds the eventTime ""
-    private static final int DIALOG_REQUEST = 9001;
+    private static final int ERROR_DIALOG_REQUEST = 9001;
 
     /******Map fields******/
     private GoogleMap mMap;
@@ -57,6 +62,9 @@ public class CreateActivity extends FragmentActivity {
     private LocationManager locManager;
     private LatLng currentLocale;
     private OnMapReadyCallback callback;
+    private GoogleApiClient mLocationClient;
+    private com.google.android.gms.location.LocationListener mListener;
+    LatLng northRidge = new LatLng(34.2417, -118.5283);
     /****End Map fields****/
 
 
@@ -66,6 +74,28 @@ public class CreateActivity extends FragmentActivity {
         setContentView(R.layout.activity_form);
 
         //---------------------------------------------------------------------------------------
+       //if statement checks to see if we are connected to the map.
+        //if we are go to Northridge first as current loc.
+        if (servicesOK()) {
+            if (initMap()) {
+                gotoLocation(34.2417, -118.5283, 15);
+
+                mLocationClient = new GoogleApiClient.Builder(this)
+                        .addApi(LocationServices.API)
+                        .addConnectionCallbacks(this)
+                        .addOnConnectionFailedListener(this)
+                        .build();
+
+                mLocationClient.connect();
+
+//                mMap.setMyLocationEnabled(true);
+            } else {
+                Toast.makeText(this, "Map not connected!", Toast.LENGTH_SHORT).show();
+            }
+
+        }//end checking to see if map is working
+
+
 
             //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -165,20 +195,49 @@ public class CreateActivity extends FragmentActivity {
             });
         }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+//------------------------------------
+    //This section is the new code added for location services on the createActivity class
+    //--------------------------------
+    public boolean servicesOK() {
+        int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
 
-        /**********Change UI settings****************/
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailable)) {
+            Dialog dialog =
+                    GooglePlayServicesUtil.getErrorDialog(isAvailable, this, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Can't connect to mapping service", Toast.LENGTH_SHORT).show();
+        }
+
+        return false;
+    }
+
+    //checks to see if the map was successfully initialized
+    private boolean initMap() {
+        if (mMap == null) {
+            SupportMapFragment mapFragment =
+                    (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+            mMap = mapFragment.getMap();
+        }
+        return (mMap != null);
+    }
+
+
+    //This method updates the maps current location
+    // Pass in a lat, lng, and zoom distance and it will move the camera to the desired location
+    private void gotoLocation(double lat, double lng, float zoom) {
+        LatLng latLng = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLng, zoom);
+        mMap.moveCamera(update);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Toast.makeText(this, "Create An Event!", Toast.LENGTH_SHORT).show();
+
+        //checks for permissions to make sure we can use the map
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -188,160 +247,42 @@ public class CreateActivity extends FragmentActivity {
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
             return;
-        }
-        mMap.setMyLocationEnabled(true); //permisions need to be set
-        mMap.getUiSettings().setMyLocationButtonEnabled(true); //false to disable
-        mMap.getUiSettings().setCompassEnabled(true);
-        mMap.getUiSettings().setZoomControlsEnabled(true);
-        mMap.getUiSettings().setRotateGesturesEnabled(true);
-        // Add a marker in Northridge and move the camera
-        /*Consider putting the following code into a method to streamline the marker placing
-        and make it more reuseable
-        */
+        }//end permission check
 
-
-        //Get an updtated location
-        locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000L, 500.0f, locationListener);
-        Location location = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
-        //If a location is found move the marker and camera to the current location
-        if ( location != null )
-        {
-            double latitude = location.getLatitude();
-            double longitude = location.getLongitude();
-            //create the location
-            currentLocale = new LatLng(latitude,longitude);
-        }//end if
-//        else
-//        {
-//            //try to get a location because none was found previously
-//            updateWithNewLocation(location);
-//        }
-
-        //wrapper around a view of a map to automatically handle the necessary life cycle needs
-        //Sets a callback object which will be triggered when the GoogleMap instance is ready to be used.
-        //May or may not need to use the OnMapReadyCallback here
-        //Turns out it was causing a problem so I took it out
-
-        /****This Arraylist Holds the FormActivity Variables****/
-        final ArrayList<String> forms = (ArrayList<String>) getIntent().getSerializableExtra("formVar");
-        //currently forms.get(0) is Event Name
-        //currently forms.get(1) is Event Type
-        //currently forms.get(2) is Event Time
-
-        //This section of code works on adding custom info window.--------------------------------
-        if(mMap != null){
-            //setinfoWindowAdapter is what we use to override Androids default popup window
-            mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
-
-                @Override
-                public View getInfoWindow(Marker marker) {
-                    return null;
-                }
-
-                //This section of code locates the contents of our custom_info_window.xml
-                //It is going to use that layout to structure our window
-                @Override
-                public View getInfoContents(Marker marker) {
-                    View v = getLayoutInflater().inflate(R.layout.custom_info_window, null);
-                    //This pulls the info from our form and populates the info window
-                    TextView tvEname = (TextView) v.findViewById(R.id.eName);
-                    TextView tvEType = (TextView) v.findViewById(R.id.eType);
-                    TextView tvETime = (TextView) v.findViewById(R.id.eTime);
-                    TextView tvOther = (TextView) v.findViewById(R.id.otherStuff);
-
-                    //this locates the position of the marker in order to put the bubble in the
-                    //correct location
-                    LatLng ll = marker.getPosition();
-
-                    //Make a parse query to get the data
-                    ParseQuery<ParseObject> query = ParseQuery.getQuery("InputForm");
-
-                    query.whereEqualTo("name", forms.get(0));
-                    query.findInBackground(new FindCallback<ParseObject>() {
-                        @Override
-                        public void done(List<ParseObject> formData, ParseException e) {
-                            if (e==null)
-                                for(int i = 0; i< formData.size(); i++)
-                                    Log.e("name", "Retrieved" + formData.get(i) + " formData");
-                            else
-                                Log.e("name", "Error: " + e.getMessage());
-                        }
-                    });
-
-                    //If form is not null then populate the info window
-                    if(forms != null) {
-                        tvEname.setText(forms.get(0));
-                        tvEType.setText(forms.get(1));
-                        tvETime.setText(forms.get(2));
-                    }
-                    return v;
-                }
-            });
-        }//end if (and end custom info window section----------------------------------------------
-
-            //This is the marker that is being used to store the data from the form
-            MarkerOptions marker = new MarkerOptions()
-                    .draggable(true)
-                    .position(currentLocale);
-
-            if(currentLocale!=null) {
-                //add marker and move camera to current location
-                mMap.addMarker(marker);
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocale));
-
-        }//end if
-
-    }//end onMapReady
-
-    //Location listener
-    //collect lat lngs and put into list
-    private final LocationListener locationListener = new LocationListener(){
-        @Override
-        public void onLocationChanged(Location location) {
-            updateWithNewLocation(location);
-            LatLng temp = new LatLng(location.getLatitude(), location.getLongitude());
-            coordinateList.add(temp);
-            listSize++;
-            Log.e("LocationListener", "Coordinates Added to list");
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-            updateWithNewLocation(null);
-        }
-
-    };
-
-    //New Location Updater
-
-    private void updateWithNewLocation(Location location) {
-        String latLongString = "";
-
-        if (location != null){
-            //create a string with current lat long
-            double lat = location.getLatitude();
-            double lng = location.getLongitude();
-            //latLongString = "Lat:" + lat + "\nLong:" + lng;
-        }
-
-        //THIS MAY NOT NEED TO BE HERE
-        //ORIGNIAL CODE HAD REDUNCANCY
-        //CHECK TO MAKE SURE
+        //mListener is going to pull the current location
+        mListener = new com.google.android.gms.location.LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                gotoLocation(location.getLatitude(), location.getLongitude(), 15);
+            }
+        };
+        //This section updates the map
+        //I used balanced power in order to get decent accuracy without sacrificing battery life
+        //If we want we can change this to low power.
+        LocationRequest request  = LocationRequest.create();
+        request.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        request.setInterval(600000);
+        request.setFastestInterval(600000);
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mLocationClient, request, mListener
+        );
 
     }
 
+    @Override
+    public void onConnectionSuspended(int i) {
 
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        LocationServices.FusedLocationApi.removeLocationUpdates(mLocationClient, mListener);
+    }
 }
 
